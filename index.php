e7384f893a399533da5a47c4dd6c833d9585c36c
Ronan MERIEN <rmerien@e2r5p6.42.fr>
2019-08-20 15:07:06 +0200

    images display

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?PHP require_once('views/v_header.php'); ?>
+<?PHP   require_once('views/v_header.php');
+        require_once('config/db_query.php'); ?>
+
+<?php       $img_list = pdo_query("SELECT * FROM image", array());
+            while ($row = $img_list->fetch()) {
+                echo "<div>";
+                echo "<img src='img/extern/".$row['image']."' >";
+                echo "<p>".$row['text']."</p>";
+                echo "</div>";
+            } ?>
 
 <?PHP require_once('views/v_footer.php'); ?>
--- views/v_upload.php
+++ views/v_upload.php
@@ -6,52 +6,43 @@
 
   // If upload button is clicked ...
   if (isset($_POST['upload'])) {
-  	// Get image name
   	$image = $_FILES['image']['name'];
-  	// Get text
   	$image_text = $_POST['image_text'];
 
   	// image file directory
-  	$target = "/camagru/img/extern/".basename($image);
+  	$target = "../img/extern/".basename($image);
 
-  	$sql = "INSERT INTO image (image, text) VALUES ('$image', '$image_text')";
+    $owner = $_SESSION['logged_on_user'];
+  	$sql = "INSERT INTO image (image, text, owner) VALUES ('$image', '$image_text', '$owner')";
   	pdo_query($sql, array());
 
   	if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
-  		$msg = "Image uploaded successfully";
+          $msg = "Image uploaded successfully";
+          header('Location: ..');
   	}else{
   		$msg = "Failed to upload image";
-  	}
+      }
+      echo $msg;
   }
-  $result = mysqli_query($db, "SELECT * FROM images");
+
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
+<head>
         <title>Upload Image | Camagru</title>
-    </head>
-    <body>
-        <div id="content">
-            <?php
-            while ($row = mysqli_fetch_array($result)) {
-            echo "<div id='img_div'>";
-            echo "<img src='images/".$row['image']."' >";
-            echo "<p>".$row['image_text']."</p>";
-            echo "</div>";
-            } ?>
-        <form method="POST" action="#" enctype="multipart/form-data">
-  	        <input type="hidden" name="size" value="1000000">
-  	        <div>
-  	            <input type="file" name="image">
-  	        </div>
-  	        <div>
-        <textarea id="text" cols="40" rows="4" name="image_text" placeholder="Caption..."></textarea>
-  	</div>
-  	<div>
-  		<button type="submit" name="upload">POST</button>
-  	</div>
-  </form>
-</div>
+</head>
+<body>
+    <form method="POST" action="#" enctype="multipart/form-data">
+        <input type="hidden" name="size" value="1000000">
+        <div>
+            <input type="file" name="image">
+        </div>
+        <div>
+            <textarea id="text" cols="40" rows="4" name="image_text" placeholder="Caption..."></textarea>
+  	    </div>  	        <div>
+        	<button type="submit" name="upload">POST</button>
+  	    </div>
+    </form>
 </body>
 </html>
 
