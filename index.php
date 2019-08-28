547df32571a0dcfb70f94f883f549f83feaefe53
Ronan MERIEN <rmerien@e1r4p3.42.fr>
2019-08-28 13:45:32 +0200

    go home

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
         require_once('config/db_query.php'); ?>
 <head>
         <title>Sign Up | Camagru</title>
-        <link rel="stylesheet" href="/public/stylesheets/feed.css">
+        <link rel="stylesheet" href="/camagru/public/stylesheets/feed.css">
 </head>
+<body>
+
 <?php       $img_list = pdo_query("SELECT * FROM image", array());
             echo "</br> </br>";
             while ($row = $img_list->fetch()) {
                 echo "<div class='feed'>";
-                echo "<img  style='max-height:300px;max-width:300px' src='img/extern/".$row['owner'].'/'.$row['image']."' >";
+                echo "<img src='img/extern/".$row['owner'].'/'.$row['image']."' >";
                 echo "<p>".$row['text']."</p>";
                 echo "</div>";
             } ?>
