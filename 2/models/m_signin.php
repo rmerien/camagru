023b52df4e3a1d53b63cee1f93082e60c4df2a56
rmerien <merienronan@gmail.com>
2019-10-21 19:07:13 +0200

    asdg

--- 2/models/m_signin.php
+++ 2/models/m_signin.php
@@ -1,5 +1,7 @@
 <?php
 
+header('Location: ../views/v_signin');
+
 session_start();
 include './class/c_user.php';
 include './class/c_database.php';
@@ -10,13 +12,10 @@ if (array_key_exists('uname', $_POST)
     try {
 		$status = User::signIn($_POST["uname"], $_POST["psw"]);
 	} catch (Exception $e) {
-		$_SESSION['error'] = "Error: " . $e->getMessage();
-		//header('Location: ../views/v_signin.php');
+		$_SESSION['error'] = "Error: gvhm" . $e->getMessage();
 	}
 	session_regenerate_id();
 	echo "Welcome back " . $_SESSION['logged_on_user']['uname'] . "!";
 }
 else
 	$_SESSION['error'] =  "Error: Missing or empty field";
-
-//header('Location: ../views/v_signin.php');
