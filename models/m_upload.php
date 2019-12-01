857c15a31a6f8bdffb5d938ee6af01fca6bc53ea
Ronan MERIEN <rppm@ronans-macbook-pro.home>
2019-12-01 13:24:47 +0100

    clean

--- models/m_upload.php
+++ models/m_upload.php
@@ -1,5 +1,8 @@
 <?php
 
+include './class/c_image.php';
+include './class/c_database.php';
+
 session_start();
 
 $uid = $_SESSION['logged_on_user']['uid'];
@@ -22,39 +25,12 @@ if ($uid) {
 
     if (!$success) {
         echo 'Upload Failed: Error code 1';
+        return;
     }
     echo $img_name;
-    //Image::addImage($uid, $img_name, $caption);
+    $success = Image::addImage($uid, $img_name, $caption);
+    if (!$success) {
+        echo 'Upload Failed: Error code 2';
+        return;
+    }
 }
-
-
-//$meta = str_replace 
-/*
-function base64ToImage($imageData, $folder){
-    list($type, $imageData) = explode(';', $imageData);
- //   $extension = explode('/',$type)[1];
-    list(,$imageData)      = explode(',', $imageData);
-    $fileName = $folder . uniqid().'.'.$extension;
-    $fileName = $folder . uniqid().'.jpeg';
-    $imageData = base64_decode($imageData);
-    $success = file_put_contents($fileName, $imageData);
-    print $success ? $fileName : 'Unable to save the file.';
-}
-*/
-/*session_start();
-
-define('UPLOAD_DIR', '../img/' . $_SESSION['logged_on_user']['username'] . '/');
-
-if (!file_exists(UPLOAD_DIR)) {
-    mkdir(UPLOAD_DIR);
-}
-
-$img = $_POST['photo'];
-
-$img = str_replace('data:image/jpeg;base64,', '', $img);
-$img = str_replace(' ', '+', $img);
-$data = base64_decode($img);
-$file = UPLOAD_DIR . uniqid() . '.jpeg';
-$success = file_put_contents($file, $data);
-print $success ? $file : 'Unable to save the file.';
-*/
